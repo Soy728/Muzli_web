8345f5c068a459dc023518f81781f3797b7fa2ec
한현우 <hyunwoo@ip-10-10-10-8.ap-northeast-2.compute.internal>
2022-09-07 16:33:50 +0900

    프로젝트 초기화

---
+++ src/components/button/index.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { ButtonProps, ButtonSize } from '.';
+
+	import './style.scss';
+	export let text: string;
+	// export let props: ButtonProps;
+	// export let size: ButtonSize;
+</script>
+
+<div class="name">
+	{text}
+	<!-- {props.text} -->
+</div>
+
+<style lang="scss" global>
+	.name {
+		color: red;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,8 @@
+<script>
+	import { Button } from '../components/button';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<div>TESTING</div>
+<Button text="ASDF" />
