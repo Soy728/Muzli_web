749eabb583873d00883ffd4c64415ff8ce5684da
sohee lee <kksm456@gmail.com>
2022-09-13 11:35:01 +0900

    button component

--- src/components/button/index.svelte
+++ src/components/button/index.svelte
@@ -3,7 +3,6 @@
 	export let text: string = '';
 	export let variant: ButtonVariant;
 	export let size: ButtonSize;
-
 </script>
 
 <div class="button" button-size={size} button-variant={variant}>
@@ -11,39 +10,38 @@
 </div>
 
 <style lang="scss">
-
 	.button {
-		margin: .5rem;
+		margin: 0.5rem;
 		text-align: center;
-    	background-color:transparent;
-    	border-radius: .5rem;
+		background-color: transparent;
+		border-radius: 0.5rem;
 
-		&[button-size="small"]{
-        width: 1rem;
-        height: 1rem;
-        font-size: 1rem;
-        border-radius: 5px;
-    }
-    	&[button-size="medium"]{
-        width: 2rem;
-        height: 2rem;
-        font-size: 2rem;
-    }
-    	&[button-size="large"]{
-        width: 3rem;
-        height: 3rem;
-        font-size: 3rem;
-    }
+		&[button-size='small'] {
+			width: 1rem;
+			height: 1rem;
+			font-size: 1rem;
+			border-radius: 5px;
+		}
+		&[button-size='medium'] {
+			width: 2rem;
+			height: 2rem;
+			font-size: 2rem;
+		}
+		&[button-size='large'] {
+			width: 3rem;
+			height: 3rem;
+			font-size: 3rem;
+		}
 
-    	&[button-variant="none"]{
-        border:0;
-    }
-   		&[button-variant="contained"]{
-        border:0;
-        background-color: var(--backgroundColor);
-    }
-    	&[button-variant="outlined"]{
-        border: .05rem solid rgb(83, 83, 83,.4);
-    }
+		&[button-variant='none'] {
+			border: 0;
+		}
+		&[button-variant='contained'] {
+			border: 0;
+			background-color: var(--backgroundColor);
+		}
+		&[button-variant='outlined'] {
+			border: 0.05rem solid rgb(83, 83, 83, 0.4);
+		}
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	import { Button } from '../components/button';
-	const text:string = "A";
+	const text: string = 'A';
 </script>
 
-<Button {text} size="small" variant="none"></Button>
-<Button {text} size="medium" variant="contained" --backgroundColor=#bae8e8></Button>
-<Button {text} size="large" variant="outlined" class = "icon-plus-lite"></Button>
-
+<Button {text} size="small" variant="none" />
+<Button {text} size="medium" variant="contained" --backgroundColor="gray" />
+<Button {text} size="large" variant="outlined" />
