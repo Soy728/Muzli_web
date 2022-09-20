8b809577924c12dc9ac3c6dfc2cb18d12ea41050
sohee lee <kksm456@gmail.com>
2022-09-20 12:01:55 +0900

    0920

--- src/routes/soy/+page.svelte
+++ src/routes/soy/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { Content } from '@src/layout/soy/content';
 	import { Header } from '@src/layout/soy/header';
-	import { CardItems } from '@src/component-route/soy/cardItems';
 </script>
 
 <Header />
